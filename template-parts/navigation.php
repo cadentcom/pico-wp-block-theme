c3252f4187f250d9ebfd0f5880d41efb51803aae
ratulkhan74 <ratul.khan503040@gmail.com>
2023-11-09 01:40:54 +0600

    setting up block theme

--- template-parts/navigation.php
+++
@@ -1,9 +0,0 @@
-<nav>
-    <?php
-    wp_nav_menu(array(
-        "theme_location" => "header_menu",
-        "menu_id" => "header_menu",
-        "menu_class" => "cadent__navigation",
-    ));
-    ?>
-</nav>
